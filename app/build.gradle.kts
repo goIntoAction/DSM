c226345f3786ae0ecd7fdcd9a740dc7eb6569bdf
Zeng Ye <czzengye@gmail.com>
2026-03-25 21:39:26 +0800

    chore: 升级版本至 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "wang.zengye.dsm"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.1.1"
+        versionCode = 5
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
